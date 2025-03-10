bdecde0f9e2f10e9839e92a39223a6bc74884bcf
yerald231ger <yerald231ger@gmail.com>
2025-03-10 17:28:35 -0600

    First commit

---
+++ Day 1/task/task.py
@@ -0,0 +1,5 @@
+print("Hello! Welcome to the Band Name Generator.")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of a pet?\n")
+BLUE = "\033[94m"
+print(f"Your band name could be {BLUE}{city} {pet}{BLUE}")
