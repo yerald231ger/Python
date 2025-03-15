c117b54fe6fefbcdc1c70b5f06dabc6c93b5a421
yerald231ger <yerald231ger@gmail.com>
2025-03-15 15:44:18 -0600

    Add .gitignore, update project files, and enhance task.py functionality

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -1,8 +1,100 @@
-word_list = ["aardvark", "baboon", "camel"]
+import random
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
+stages = [r'''
+  ┟───┐
+  ┊   │
+  O   │
+ /│\  │
+ / \  │
+      │
+▔▔▔▔▔▔▔▔▔
+''', r'''
+  ┟───┐
+  ┊   │
+  O   │
+ /│\  │
+ /    │
+      │
+▔▔▔▔▔▔▔▔▔
+''', r'''
+  ┟───┐
+  ┊   │
+  O   │
+ /│\  │
+      │
+      │
+▔▔▔▔▔▔▔▔▔
+''', '''
+  ┟───┐
+  ┊   │
+  O   │
+ /│   │
+      │
+      │
+▔▔▔▔▔▔▔▔▔''', '''
+  ┟───┐
+  ┊   │
+  O   │
+  │   │
+      │
+      │
+▔▔▔▔▔▔▔▔▔
+''', '''
+  ┟───┐
+  ┊   │
+  O   │
+      │
+      │
+      │
+▔▔▔▔▔▔▔▔▔
+''', '''
+  ┟───┐
+  ┊   │
+      │
+      │
+      │
+      │
+▔▔▔▔▔▔▔▔▔
+''']
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+RED_START = "\033[1;31m"
+RED_END = "\033[0;0m"
+BLUE_START = "\033[1;34m"
+BLUE_END = "\033[0;0m"
+word_list = ["hamstrings"]
 
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
-#  is, "Wrong" if it's not.
+chosen_word = random.choice(word_list)
+print("\nThe Hangman Game")
+
+placeholder = ["_"] * len(chosen_word)
+print()
+tries = len(stages) - 1
+
+while tries >= 0:
+
+    guess = input("Guess a letter: ").lower()
+
+    for index, letter in enumerate(chosen_word):
+        if letter == guess:
+            placeholder[index] = letter
+
+    print(f"\nYou have {tries} tries left.")
+
+    displayed = "".join(placeholder)
+
+    print()
+    print("┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅")
+
+    print(stages[tries])
+    if guess not in chosen_word:
+        tries -= 1
+        print(f"{RED_START}the letter is not in the word{RED_END}")
+
+    print(" " + BLUE_START + displayed + BLUE_END)
+    print("┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅")
+
+    if displayed == chosen_word:
+        print("\nYou WIN.")
+        exit(0)
+
+print("\nYou lose.")
--- Day 7/task/solution.py
+++ Day 7/task/solution.py
@@ -1,13 +1,100 @@
 import random
+stages = [r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 word_list = ["aardvark", "baboon", "camel"]
+
+lives = 6
 
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
-guess = input("Guess a letter: ").lower()
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print(placeholder)
 
-for letter in chosen_word:
-    if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+game_over = False
+correct_letters = []
+
+while not game_over:
+    guess = input("Guess a letter: ").lower()
+
+    display = ""
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print(display)
+
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            game_over = True
+            print("You lose.")
+
+    if "_" not in display:
+        game_over = True
+        print("You win.")
+
+    print(stages[lives])
