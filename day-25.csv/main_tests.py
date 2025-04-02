0e92d096117e4e978e80c146de57d17518380507
yerald231ger <yerald231ger@gmail.com>
2025-04-02 13:39:18 -0600

    Refactor day-21 directory by removing additional unnecessary files to further streamline project structure

---
+++ day-25.csv/main_tests.py
@@ -0,0 +1,17 @@
+import csv
+import pandas as pd
+
+data = pd.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+
+gray_squirrels = len(data[data["Primary Fur Color"] == "Gray"])
+cinnamon_squirrels = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels = len(data[data["Primary Fur Color"] == "Black"])
+
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [gray_squirrels, cinnamon_squirrels, black_squirrels]
+}
+
+data = pd.DataFrame(data_dict)
+data.to_csv("squirrel_count.csv")
